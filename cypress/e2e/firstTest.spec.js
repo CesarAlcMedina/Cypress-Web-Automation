07a8cc4bd44a0c8411bdc6712837611fdc6cefbe
CesarAlcMedina <djcesar01@hotmail.com>
2025-08-05 14:36:08 -0400

    First page object

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -196,7 +196,7 @@ describe("First test suit", () => {
         cy.get('nb-layout-column').click()
     })
 
-    it.only('Web tables', () => {
+    it('Web tables', () => {
         cy.visit('/')
         cy.contains('Tables & Data').click()
         cy.contains('Smart Table').click()
@@ -237,5 +237,49 @@ describe("First test suit", () => {
         })
         })
     })
+
+
+    it('Tooltip', () => {
+
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Tooltip').click()
+
+        cy.contains('nb-card', 'Colored Tooltips')
+            .contains('Default').click()
+        
+        cy.get('nb-tooltip').should('contain', 'This is a tooltip')    
+
+
+    })
+
+    it('Dialog box', () => {
+        cy.visit('/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+
+        //1
+
+        cy.get('tbody tr').first().find('.nb-trash').click()
+        cy.on('window: confirm', (confirm) => {
+            expect(confirm).to.equal('Are you sure you want to delete?')
+        })
+        
+        //2
+
+        const stub = cy.stub()
+
+        cy.on('window:confirm', stub)
+        cy.get('tbody tr').first().find('.nb-trash').click().then(() =>{
+            expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+        })
+
+        //3
+        cy.get('tbody tr').first().find('.nb-trash').click()
+        cy.on('window:confirm', () => true)
+
+    })
+
 
 })
