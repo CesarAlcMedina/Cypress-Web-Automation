9e4298524eb8925bf50ac14bd6e9c7b8f272c242
CesarAlcMedina <djcesar01@hotmail.com>
2025-08-06 14:14:00 -0400

    first page object1

---
+++ cypress/e2e/testWithPageObjects.js
@@ -0,0 +1,16 @@
+import {navigateTo} from '../support/page_object/navigationPage'
+
+
+describe('Tes with page objects', () => {
+
+    beforeEach('open application', () => {
+        cy.visit('/')
+    })
+
+
+    it('Verify navigations acrross the pages', () => {
+
+        navigateTo.formsLayoutsPage()
+        navigateTo.datepickerPage()
+    })
+})
